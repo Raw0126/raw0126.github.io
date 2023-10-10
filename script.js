3df9cf607c0a3ebaa0315ea2463846d169d34fd5
Raw0126 <146454204+Raw0126@users.noreply.github.com>
2023-10-10 13:59:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,25 @@
+const filterButtons = document.querySelectorAll(".filter_buttons button");
+const filterablecards = document.querySelectorAll(".filterable_cards .card");
 
-filterObjects ("all");
 
 
-function filterObjects(c) {
-    var x,i;
 
-    if( c =="all") c ="";
-        for(i=0; i< x.length; i++){
-            removeClass(x[i], "show");
-            if(x[i].className.indexOf(c) > -1) addClass(x[i], "show")
-        }
+const filterCards = e => {
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+   
 
+    filterablecards.forEach(card => {
+        card.classList.add("hide");
+
+        if (card.dataset.name === e.target.dataset.name || e.target.dataset.name === "dura"){
+        card.classList.remove("hide");
     }
-    
 
-    function addClass(element,name){
-        var i, arr1,arr2;
-
-        arr1= element.className.split("");
-        arr2 = name.split("");
-
-        for(i=0; i< arr2.length; i++){
-            
-            if(arr1.indexOf(arr2[i] )== -1) {
-                element.className += " " + arr2[i];
-            }
-        }
+    });
 
 
+};
 
-        function removeClass( element , name){
-            var i, arr1,arr2;
+filterButtons.forEach(button => button.addEventListener("click",filterCards));
 
-        arr1= element.className.split("");
-        arr2 = name.split("");
-
-        for(i=0; i< arr2.length; i++){
-            while (arr1.indexOf(arr2[i]) > -1){
-                arr1.splice(arr1.indexOf(arr2[i]),1);
-            }
-
-        }
-
-    }
-    element.className =arr1.join("");
-    
-}
