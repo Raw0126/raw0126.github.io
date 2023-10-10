2e3f4ce84d14233fe0fa3b377e889820ec0a53a0
Raw0126 <146454204+Raw0126@users.noreply.github.com>
2023-10-10 14:14:02 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,25 @@
+const filterButtons = document.querySelectorAll(".filter_buttons button");
+const filterablecards = document.querySelectorAll(".filterable_cards .card");
+
+
+
+
+const filterCards = e => {
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+   
+
+    filterablecards.forEach(card => {
+        card.classList.add("hide");
+
+        if (card.dataset.name === e.target.dataset.name || e.target.dataset.name === "dura"){
+        card.classList.remove("hide");
+    }
+
+    });
+
+
+};
+
+filterButtons.forEach(button => button.addEventListener("click",filterCards));
+
